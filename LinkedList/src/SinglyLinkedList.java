2339a18a62273c5e0398bb23f07e87440bf03f43
Girish <girish12ramesh@gmail.com>
2017-10-20 19:43:52 -0700

    feat: Switch case

--- LinkedList/src/SinglyLinkedList.java
+++ LinkedList/src/SinglyLinkedList.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 
 public class SinglyLinkedList {
-
+public static void main(){
+	Scanner scan = new Scanner(System.in);
+	//Create an object of class linkedList
+	linkedList list =new linkedList();
+	System.out.println("Singly Linked List Test");
+	char ch = 0;
+	int choice=scan.nextInt();
+	do{
+		switch(choice){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+			default : System.out.println();
+			break;
+		}
+		
+	}while(ch=='Y' || ch =='y');
+	
+}
 }
