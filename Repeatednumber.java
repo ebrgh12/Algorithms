775fb9eed3c874cdfc84ab11db468473e05c3c34
Girish <girish12ramesh@users.noreply.github.com>
2018-03-13 21:19:31 -0700

    Find the repeated number

---
+++ Repeatednumber.java
@@ -0,0 +1,31 @@
+public class MyClass {
+  
+  public static void main(String args[]) 
+{
+   
+     int []arr ={1,2,2,3,4};
+      
+	  int n=0;
+       
+	  Arrays.sort(arr);
+  
+          int len=arr.length;
+   
+	  n=len-1;
+        
+	 int target=(n^2+n)/2;
+    
+	 int arraySum=0;
+       
+	 for(int i=0;i<len;i++){
+      
+	      arraySum=arraySum+i;
+   
+         }
+        
+	System.out.print(arraySum-target);
+    
+    
+    }
+
+}
